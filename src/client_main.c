77150bb8363e729c6cd38d87fce1d9ba6862d7b3
Valentin LEPILLER <vlepille@e1r9p9.clusters.42paris.fr>
2022-12-30 16:16:41 +0100

    Implements first communication between the erver and the client

--- src/client_main.c
+++ src/client_main.c
@@ -1,11 +1,30 @@
 #include <signal.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <stdlib.h> // @TODO rm and replace by my atoi
 #include <stdio.h> // @TODO rm and replace by my ft_printf or use write
+
 int	main(int argc, char **argv)
 {
-	if (argc != 2)
-		return ((printf("You need 1 arg"), 0));
-	printf("You select PID %d", atoi(argv[1])); // @TODO RM
-	kill(atoi(argv[1]), SIGUSR1);
+	int	i;
+	int	j;
+
+	if (argc != 3)
+		return ((printf("You need 2 arg"), 0));
+	i = 0;
+	j = 0;
+	while (!i || argv[2][i - 1])
+	{
+		j = 0;
+		while (j < 8)
+		{
+			if (!((argv[2][i] >> j) & 1))
+				kill(atoi(argv[1]), SIGUSR1);
+			else
+				kill(atoi(argv[1]), SIGUSR2);
+			j++;
+			sleep(1 / 100);
+		}
+		i++;
+	}
 }
--- src/server_main.c
+++ src/server_main.c
@@ -1,20 +1,56 @@
+#include "server.h"
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
 // @TODO rm bellow
 #include <stdio.h> // @TODO replace by my printf
-int	waiting = 1;
 
-void	handle(int sig)
+void	handler(int sig)
 {
-	(void) sig;
-	waiting = 0;
+	static int		i;
+	static char		receive;
+
+	if (sig != SIGUSR1 && sig != SIGUSR2)
+		return ;
+	receive += (sig == SIGUSR2) << i;
+	i++;
+	if (i == 8)
+	{
+		if (!receive)
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			receive = 0;
+			i = 0;
+			return ;
+		}
+		printf("<%d>\n", receive);
+		write(STDOUT_FILENO, &receive, 1);
+		receive = 0;
+		i = 0;
+	}
 }
 
 int	main(void)
 {
+	struct sigaction	act;
+
 	printf("My PID is: %d\n", getpid());
-	signal(SIGUSR1, handle);
-	while (waiting)
+
+	act.sa_handler = handler;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	//sigaddset(set, SIGUSR1); // Used to block signals @TODO TRY
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+
+	while (1)
 		;
 }
+//sigemptyset
+//sigaddset
+//sigaction
+
+//pause
+//sleep
+//usleep
+//exit
