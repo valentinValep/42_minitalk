c4f39666869adde0b66bd0a9564f19f572d7bf2a
Valentin LEPILLER <vlepille@e2r13p6.clusters.42paris.fr>
2023-01-10 10:40:33 +0100

    Surrend this too many complex implementation.

--- src/server.h
+++ src/server.h
@@ -1,6 +1,13 @@
 #ifndef SERVER_H
 # define SERVER_H
 
-# define _POSIX_SOURCE
+# include <sys/types.h>
+
+typedef struct s_client_pid
+{
+	pid_t	pid;
+	int		is_full;
+	int		i;
+}	t_client_pid;
 
 #endif
--- src/server_main.c
+++ src/server_main.c
@@ -1,30 +1,57 @@
 #include "server.h"
-#include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
 // @TODO rm bellow
 #include <stdio.h> // @TODO replace by my printf
 
+void	reset_client_pid(t_client_pid *client_pid)
+{
+	client_pid->pid = 0;
+	client_pid->is_full = 0;
+	client_pid->i = 0;
+}
+
+void	get_client_pid(t_client_pid *client_pid, int sig)
+{
+	client_pid->pid += (sig == SIGUSR2) << client_pid->i;
+	client_pid->i++;
+	if (client_pid->i == sizeof(pid_t) * 8)
+	{
+		printf("CLIENT_PID: %d\n",client_pid->pid);
+		if (kill(client_pid->pid, SIGUSR1))
+			reset_client_pid(client_pid);
+		else
+			client_pid->is_full = 1;
+	}
+}
+
 void	handler(int sig)
 {
-	static int		i;
-	static char		receive;
+	static int			i;
+	static char			receive;
+	static t_client_pid	client_pid;
 
 	if (sig != SIGUSR1 && sig != SIGUSR2)
 		return ;
+	if (!client_pid.is_full)
+	{
+		i = 0;
+		get_client_pid(&client_pid, sig);
+		return ;
+	}
 	receive += (sig == SIGUSR2) << i;
 	i++;
 	if (i == 8)
 	{
-		if (!receive)
+		if (receive)
+		{
+			write(STDOUT_FILENO, &receive, 1);
+		}
+		else
 		{
 			write(STDOUT_FILENO, "\n", 1);
-			receive = 0;
-			i = 0;
-			return ;
+			reset_client_pid(&client_pid);
 		}
-		printf("<%d>\n", receive);
-		write(STDOUT_FILENO, &receive, 1);
 		receive = 0;
 		i = 0;
 	}
@@ -38,19 +65,12 @@ int	main(void)
 
 	act.sa_handler = handler;
 	act.sa_flags = 0;
+	sigaddset(&act.sa_mask, SIGUSR1); // Used to block signals @TODO TRY
+	//sigaddset(&act.sa_mask, SIGUSR2); // Used to block signals @TODO TRY
 	sigemptyset(&act.sa_mask);
-	//sigaddset(set, SIGUSR1); // Used to block signals @TODO TRY
 	sigaction(SIGUSR1, &act, NULL);
 	sigaction(SIGUSR2, &act, NULL);
 
 	while (1)
 		;
 }
-//sigemptyset
-//sigaddset
-//sigaction
-
-//pause
-//sleep
-//usleep
-//exit
