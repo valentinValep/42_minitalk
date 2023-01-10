6c94b5de8617bc2c4017f7a380d86ead673c5e5c
Valentin LEPILLER <vlepille@e2r13p6.clusters.42paris.fr>
2023-01-10 11:41:09 +0100

    Implement client base behavior

--- src/client_main.c
+++ src/client_main.c
@@ -1,5 +1,29 @@
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+// @TODO rm include bellow
+#include <stdlib.h>
+void	send_char(__pid_t pid, char c)
+{
+	int	i;
+
+	i = -1;
+	while (++i < (int)sizeof(char) * 8)
+	{
+		kill(pid, (int []){SIGUSR1, SIGUSR2}[(c >> i) & 1]);
+		usleep(10000);
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
+	int	i;
+
+	if (argc != 3)
+		return ((write(STDOUT_FILENO, "You need 2 args : server pid and a string", 41), 0));
+
+	// @TODO verif argv[1] is a pid
+	i = -1;
+	while ((i == -1 && i++) || argv[2][i++])
+		send_char(atoi(argv[1]), argv[2][i]);
 }
--- src/server_main.c
+++ src/server_main.c
@@ -1,8 +1,20 @@
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
 // @TODO rm includes bellow
 #include <stdio.h>
+
+void	handler(int sig)
+{
+	printf("Signal : %d\n", sig);
+}
+
 int	main(void)
 {
 	printf("Program PID : %d\n", getpid());
+
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	while (1)
+		pause();
 }
