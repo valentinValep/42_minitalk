b25809839148100ad6d590cfdddc99f90ce2057c
Valentin LEPILLER <vlepille@student.42.fr>
2023-01-10 15:23:19 +0100

    Add a buffer to print the received message and update libft

--- src/server_main.c
+++ src/server_main.c
@@ -1,22 +1,33 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "libft.h"
 void	handler(int sig)
 {
-	static int	i;
-	static char	c;
+	static int		i;
+	static char		c;
+	static t_string	str;
 
 	if (sig != SIGUSR1 && sig != SIGUSR2)
 		return ;
 	c += (sig == SIGUSR2) << i;
 	i++;
+	if (!str.str && ft_init_string(&str, NULL))
+		exit(1);
 	if (i == sizeof(char) * 8)
 	{
 		if (c)
-			write(STDOUT_FILENO, &c, 1);
+		{
+			if (ft_cat_string(&str, (char []){c, 0}))
+				exit(1);
+		}
 		else
+		{
+			write(STDOUT_FILENO, str.str, str.strlen);
 			write(STDOUT_FILENO, "\n", 1);
+			ft_destroy_string(&str);
+		}
 		i = 0;
 		c = 0;
 	}
