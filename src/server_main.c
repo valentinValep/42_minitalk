0a0ed2ef3f6d011810709004320d730f286d61fe
Valentin LEPILLER <vlepille@e2r13p6.clusters.42paris.fr>
2023-01-10 10:56:43 +0100

    Add print of pid of teh server

--- src/server_main.c
+++ src/server_main.c
@@ -1,4 +1,8 @@
-int	main(int argc, char **argv)
+#include <sys/types.h>
+#include <unistd.h>
+// @TODO rm includes bellow
+#include <stdio.h>
+int	main(void)
 {
-	//
+	printf("Program PID : %d\n", getpid());
 }
